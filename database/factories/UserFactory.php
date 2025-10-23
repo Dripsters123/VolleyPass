363cafcc69b43f240b0d0435633fb55ab114ce95
Dripsters123 <ipb22.k.skrastins@vtdt.edu.lv>
2025-10-23 19:21:03 +0300

    trying to fix the error in the server

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,9 +29,16 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'role' => 'user',
         ];
     }
-
+public function admin(): static
+{
+    return $this->state(fn (array $attributes) => [
+        'role' => 'admin',
+        'name' => 'Admin User',
+    ]);
+    }
     /**
      * Indicate that the model's email address should be unverified.
      */
